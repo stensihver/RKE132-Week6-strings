873e09e4c89904b73f4ab11148bf2c8fdc0baec8
stensihver <sten.sihver@tktk.ee>
2024-10-09 18:54:52 +0300

    add week6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chiken wings", "asian takeaway" };
+
+Random rnd = new Random();  
+
+int randomIndex = rnd.Next(snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}");
---
+++ Week6StringsAndArreys/Program.cs
@@ -0,0 +1,24 @@
+﻿string hello = "We are the champions?";
+
+int stringlength  = hello.Length;
+
+hello = hello.Trim();
+
+char firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstletter = hello[0]; 
+Console.WriteLine(firstletter); 
+
+Console.WriteLine(hello);
+
+int wordcounter = 0;
+
+for (int i = 0; i < hello.Length; i++)
+{
+    Console.WriteLine($"{i} symbol in string: {hello[i]}");
+}
+
+Console.WriteLine(stringlength);
